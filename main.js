92e260bb0208078d24c53dc65261c23dc03adef9
bekzat <bekzatajtbekzanov@gmail.com>
2024-10-07 10:53:00 +0600

    f

--- main.js
+++ main.js
@@ -6,37 +6,52 @@ const content = document.querySelector(".content");
 
 function searchDayName() {
   let inputValue = input.value;
-  let nameDay = null;
-  switch (inputValue) {
-    case "1":
-      nameDay = "Понедельник";
-      break;
-    case "2":
-      nameDay = "Вторник";
-      break;
-    case "3":
-      nameDay = "Cреда";
-      break;
-    case "4":
-      nameDay = "Четверг";
-      break;
-    case "5":
-      nameDay = "Пятница";
-      break;
-    case "6":
-      nameDay = "Суббота";
-      break;
-    case "7":
-      nameDay = "Воскресенье";
-      break;
-    default:
-      nameDay = "Такое название дня не существует";
-  }
-  content.innerHTML += `<div class = 'box'>${nameDay}</div>`;
 
-  setTimeout(() => {
-    content.innerHTML = ""
-  }, 2000);
+  let nameDay = null;
+  if (inputValue.length) {
+    switch (inputValue) {
+      case "1":
+        nameDay = "Понедельник";
+        break;
+      case "2":
+        nameDay = "Вторник";
+        break;
+      case "3":
+        nameDay = "Cреда";
+        break;
+      case "4":
+        nameDay = "Четверг";
+        break;
+      case "5":
+        nameDay = "Пятница";
+        break;
+      case "6":
+        nameDay = "Суббота";
+        break;
+      case "7":
+        nameDay = "Воскресенье";
+        break;
+      default:
+        nameDay = "Такое название дня не существует";
+    }
+    content.innerHTML += `<div class = 'box'>${nameDay}</div>`;
+
+    setTimeout(() => {
+      content.innerHTML = "";
+    }, 2000);
+  }
 }
 
 btn.addEventListener("click", searchDayName);
+
+let user = prompt("введите число ");
+
+let numm = Number(user);
+
+if (numm > 0) {
+  console.log("Число положительное.");
+} else if (numm == 0) {
+  console.log("Число ровняется 0");
+} else {
+  console.log("Число отрецательное");
+}
